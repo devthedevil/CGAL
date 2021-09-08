75e1a3ac4f9b46f9c935e2f0b9d00f353dada3dd
devthedevil <41816786+devthedevil@users.noreply.github.com>
2021-09-08 22:11:31 +0530

    Add files via upload

---
+++ demo/CMakeFiles/Points.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Points.dir/Points.cpp.o"
+  "Points.pdb"
+  "Points"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Points.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
